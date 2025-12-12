c222e4aab9ca970cd14765f6d7e72c780582960a
OrlandoCV <orlandocv0107@gmail.com>
2025-12-12 18:09:09 -0300

    resolver : Category

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -31,12 +31,16 @@ import { Role } from '../organization/entities/enums';
 
 @ApiTags('Categories')
 @Controller('organizations/:organizationId/categories')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@ApiBearerAuth('access-token')
 export class CategoriesController {
   constructor(private readonly service: CategoriesService) {}
+
   // --------------------------------------------------------
   // GET ALL
   // --------------------------------------------------------
   @Get()
+  //@Roles(Role.ADMIN, Role.SUPERADMIN, Role.EDITOR, Role.VISITOR)
   @ApiOperation({
     summary: 'Listar categorías',
     description:
